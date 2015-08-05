a8a2c138804a28b99e7dea5c8f3d0d85fb6533f3
Adam Incera <aji2112@columbia.edu>
2015-08-04 23:38:36 -0400

    shifted everything up a little

--- assets/js/organize.js
+++ assets/js/organize.js
@@ -1,7 +1,7 @@
 function organize() {
 	$(function() {
-		var navHeight = (window.innerHeight - $('#nav').height()) / 2;
-		var contentHeight = (window.innerHeight - $('#content').height()) / 2;
+		var navHeight = (window.innerHeight - $('#nav').height()) / 2 * 0.8;
+		var contentHeight = (window.innerHeight - $('#content').height()) / 2 * 0.8;
 		$('#nav').offset({top: navHeight, left: 10});
 		$('#content').offset({top: contentHeight});
 	});
