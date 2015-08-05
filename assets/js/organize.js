70e16c27e0e2c9b9b28c3739c15dbe6781c24106
Adam Incera <aji2112@columbia.edu>
2015-08-05 00:23:40 -0400

    added some hipster ipsum

--- assets/js/organize.js
+++ assets/js/organize.js
@@ -2,6 +2,7 @@ function organize() {
 	$(function() {
 		var navHeight = (window.innerHeight - $('#nav').height()) / 2 * 0.8;
 		var contentHeight = (window.innerHeight - $('#content').height()) / 2 * 0.8;
+		if(contentHeight < 10) contentHeight = 10;
 		$('#nav').offset({top: navHeight, left: 10});
 		$('#content').offset({top: contentHeight});
 	});
